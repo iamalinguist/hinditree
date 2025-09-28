e716777f20ad2fac7c1f911f111d7069cfb3b52f
Rathod Dinesh <dinesh.rathod.c2019@iitbombay.org>
2025-09-28 12:55:16 +0530

    update package dependencies and configuration

--- HindiTreeApp/vite.config.ts
+++ HindiTreeApp/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
+// Set base to repo name for GitHub Pages
 export default defineConfig({
+  base: '/HindiTreeApp/',
   plugins: [react(), tailwindcss()],
-})
+});
